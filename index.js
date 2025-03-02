db94187365a9ac83837a0438555f8b819d4dace3
Subha Maity <maitysubha1321@gmail.com>
2025-03-02 21:18:25 +0530

    testing

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ console.log(apiKey);
 
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500/");
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Credentials", "true");
